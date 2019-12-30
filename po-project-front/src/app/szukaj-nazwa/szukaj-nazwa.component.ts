3f1178250d2e020a3f1b7a7b66dd1bbabf0ee251
mateusz.koza <32997806+Chrumcio@users.noreply.github.com>
2019-12-30 12:57:36 +0100

    fixed bugs in components created form service for getting data from database

--- po-project-front/src/app/szukaj-nazwa/szukaj-nazwa.component.ts
+++ po-project-front/src/app/szukaj-nazwa/szukaj-nazwa.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { KontoSerwis } from '../serwis/konto.serwis';
+import { Location } from '@angular/common';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-szukaj-nazwa',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SzukajNazwaComponent implements OnInit {
 
-  constructor() { }
+  nazwa: string;
+
+  @ViewChild("szukanieNazwaForm",{static: false}) formularz: NgForm;
+
+  constructor(private route: ActivatedRoute, private router: Router, private kontoSerwis: KontoSerwis, private _location: Location) { }
 
   ngOnInit() {
+  }
+
+  szukaj() {
+    if(this.formularz.valid){
+      this.nazwa = this.formularz.controls.nazwa.value;
+      console.log(this.nazwa);
+    }
   }
 
 }
