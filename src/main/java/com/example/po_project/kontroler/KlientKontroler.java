6d866360a7731c7fe2085a6831b6c9a93d0c4d41
mateusz.koza <32997806+Chrumcio@users.noreply.github.com>
2020-01-13 00:15:45 +0100

    created klient full backend

--- src/main/java/com/example/po_project/kontroler/KlientKontroler.java
+++ src/main/java/com/example/po_project/kontroler/KlientKontroler.java
@@ -1,7 +1,23 @@
 package com.example.po_project.kontroler;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.po_project.model.Klient;
+import com.example.po_project.serwis.KlientSerwis;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/klient")
+@CrossOrigin
 public class KlientKontroler {
+
+    private KlientSerwis klientSerwis;
+
+    public KlientKontroler(KlientSerwis klientSerwis) {
+        this.klientSerwis = klientSerwis;
+    }
+
+    @GetMapping("/kontoid/{id}")
+    public ResponseEntity<Klient> getKlientByKontoId(@PathVariable(value = "id")Long id){
+        return ResponseEntity.ok(klientSerwis.getKlientByKontoId(id));
+    }
 }
--- src/main/java/com/example/po_project/serwis/KlientSerwis.java
+++ src/main/java/com/example/po_project/serwis/KlientSerwis.java
@@ -1,7 +1,19 @@
 package com.example.po_project.serwis;
 
+import com.example.po_project.model.Klient;
+import com.example.po_project.repozytorium.KlientRepozytorium;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KlientSerwis {
+
+    private KlientRepozytorium klientRepozytorium;
+
+    public KlientSerwis(KlientRepozytorium klientRepozytorium) {
+        this.klientRepozytorium = klientRepozytorium;
+    }
+
+    public Klient getKlientByKontoId(Long id){
+        return klientRepozytorium.findKlientByKontoid(id);
+    }
 }
